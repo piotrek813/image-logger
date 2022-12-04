35c186d1e9372b79c237c60acca0a63882271956
peters04 <piotreksmolinski04@gmail.com>
2022-12-04 11:16:58 +0100

    log to file

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var morgan = require("morgan");
+var fs = require("fs");
 
 var indexRouter = require("./routes/index");
+var logRouter = require("./routes/log");
 
 var app = express();
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
 
-app.use(logger("dev"));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/log", logRouter);
 
 module.exports = app;
 
